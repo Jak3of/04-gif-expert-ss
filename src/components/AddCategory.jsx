29e73f228a6d313a4d4505e935fb359c20ac616c
Nelson Correa <nelsonc897@gmail.com>
2024-03-17 17:17:42 -0500

    First Commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export const AddCategory = ( { onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('One Punch');
+
+    const onInputChange = ({ target: { value } }) => {
+        setInputValue(value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputValue);
+        if (inputValue.trim().length <= 1) return;
+        
+        // setCategories(categories => [ ...categories , inputValue]);
+
+        // emite el nuevo valor
+        onNewCategory(inputValue.trim());
+        setInputValue('');
+        
+    }
+
+
+
+
+    return (
+        <form className="flex gap-2 items-center justify-center" onSubmit={onSubmit} >
+            <input type="text" placeholder="Buscar Gifs" id="search" onChange={onInputChange} value={inputValue}  />
+            <button type="submit"  className="p-3 border-1 border-black rounded-lg bg-slate-500 text-white ">Add</button>
+        </form>
+    )
+}
---
+++ src/components/GifItem.jsx
@@ -0,0 +1,14 @@
+
+export const GifItem = ({url , title , id}) => {
+
+    return (
+        <div className="card">
+            
+            <img src={url} alt={title} />
+
+                        
+            <p>{title}</p>
+            
+        </div>
+    )
+}
